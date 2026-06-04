5f48a72a4064a5f990742e0d1ad77dfe2753b35d
vamshigogu <vamshi.gogu@yahoo.com>
2026-06-04 06:39:49 +0530

    created  home components and added basic home layout info

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Sidebar],
+  imports: [Sidebar,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
